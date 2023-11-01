de1e19b7675f18e97d440cdcdb49d53af5a3bda4
Big-Logic <angwayah21@gmail.com>
2023-11-01 12:50:16 +0000

    change cors origin

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const helmet = require("helmet");
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://theviewerhub.onrender.com",
   credentials: true,
 };
 
